591ce43a584af8fa2fbbbb748f764d103684d6e7
N.Clem <sonche16@hotmail.com>
2023-04-26 15:52:59 +0200

    added a form to the contact page

--- basic-site-v01/resources/views/contact.blade.php
+++ basic-site-v01/resources/views/contact.blade.php
@@ -4,4 +4,27 @@
 
 @section('content')
     <h1>Contact</h1>
+    <form method="post" action="{{ route('contact.submit') }}" class="row g-3" >
+        @csrf
+
+        <div class="col-md-6">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" class="form-control" id="name" name="name" required>
+        </div>
+
+        <div class="col-md-6">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email" name="email" required>
+        </div>
+
+        <div class="form-floating">
+            <textarea class="form-control" placeholder="Leave a message here" id="message" name="message" required></textarea>
+            <label for="message">Message</label>
+        </div>
+
+        <div class="col-auto">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </form>
+    
 @stop
